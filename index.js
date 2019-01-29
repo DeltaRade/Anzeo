a88f68ee41d802b6957473621f23d49ef406e5ba
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-01-28 20:48:43 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const liberch = require('liberch');
 const client = new liberch.Client({ prefixes:['Li', '.', '&'], ownerID:'298258003470319616', mentionAsPrefix:true });
 client.loadCommands('commands');
 client.loadEvents('events');
-
 client.events.on('commandError', (error)=>{
 	console.log(error);
 });
-
 const filewatch = new liberch.FileWatch();
 filewatch.watchDir('commands');
 filewatch.on('dirChanged', (event, dir, file)=>{
@@ -15,4 +13,5 @@ filewatch.on('dirChanged', (event, dir, file)=>{
 	console.log('reloading files');
 	client.reloadCommand(`${dir}/${file}`);
 });
+
 client.login(process.env.token);// process.env.token
