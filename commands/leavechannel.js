b65e08c49b21057c23bc4eb18242cd736d4edb62
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-02-10 10:18:26 -0400

    Add files via upload

--- commands/leavechannel.js
+++ commands/leavechannel.js
@@ -1,18 +1,15 @@
-const { Command, PostgreSQL } = require('liberch');
+const { Command, SQLite3 } = require('liberch');
 class LChannel extends Command {
 	constructor() {
 		super({ name:'leavechannel', alias:['lchannel'] });
 	}
 
 	async execute(client, message) {
-		const sql = new PostgreSQL({
-			connectionString:process.env.DATABASE_URL,
-			ssl:true,
-		});
-		await sql.connect();
+		const sql = new SQLite3('settings.sqlite');
 		const channel = message.channel;
-		await sql.upsert('settings', ['guild', 'leavechannel'], [message.guild.id, channel.id]);
-		await sql.end();
+		await sql.insertIgnore('settings', ['guild', 'leavechannel'], [message.guild.id, channel.id]);
+		await sql.update('settings', 'leavechannel', message.channel.id, 'guild', message.guild.id);
+		await sql.close();
 		message.channel.send('leave channel selected');
 	}
 }
--- commands/leavemsg.js
+++ commands/leavemsg.js
@@ -5,22 +5,18 @@ class Leavemsg extends liberch.Command {
 	}
 
 	async execute(client, message, args) {
-		const sqlite = new liberch.PostgreSQL({
-			connectionString:process.env.DATABASE_URL,
-			ssl:true,
-		});
-		await sqlite.connect();
+		const sqlite = new liberch.SQLite3('settings.sqlite');
 		if(!args[0]) {
 			message.channel.send('disabled leave message');
-			await sqlite.upsert('settings', ['guild', 'leavemsg'], [message.guild.id, '']);
-			// await sqlite.update('settings', 'leavemsg', '', 'guild', message.guild.id);
-			await sqlite.end();
+			await sqlite.update('settings', 'leavemsg', '', 'guild', message.guild.id);
+			await sqlite.close();
 			return;
 		}
 		const msg = args.join(' ');
 		const guildID = message.guild.id;
-		await sqlite.upsert('settings', ['guild', 'leavemsg'], [guildID, msg]);
-		await sqlite.end();
+		await sqlite.insertIgnore('settings', ['guild', 'leavemsg'], [guildID, msg]);
+		await sqlite.update('settings', 'leavemsg', msg, 'guild', guildID);
+		await sqlite.close();
 		message.channel.send(`leave message selected\npreview:\n\`\`\`${msg}\`\`\``);
 	}
 }
