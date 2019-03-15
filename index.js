0c2515a9591f914e76d45e01994b1b6f0ea22e19
DeltaRade <coolxpress26@gmail.com>
2019-03-15 19:47:36 -0400

    improvements

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const liberch = require('liberch');
 const client = new liberch.Client({ prefixes:['Li', '.'], ownerID:'298258003470319616', mentionAsPrefix:true });
-client.loadCommands('commands');
+client.commandHandler.load('commands')
 client.loadEvents('events');
-client.events.on('commandError', (error)=>{
+client.commandHandler.on('commandError', (error)=>{
 	console.log(error);
 });
-
-const filewatch = new liberch.FileWatch();
-filewatch.watchDir('commands');
-filewatch.on('dirChanged', (event, dir, file)=>{
-	console.log(event);
-	if(event != 'change') {return;}
-	console.log('reloading files');
-	client.reloadCommand(`${dir}/${file}`);
-});
+client.on('message',(message)=>{
+	client.commandHandler.exec(message)
+})
 client.login(process.env.token);// process.env.token
